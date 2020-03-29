659f67c139abee4a76acc6e2c29f98e26f32360e
regalforlyfe <rifal.ahmd2@gmail.com>
2020-03-29 21:28:01 +0700

    Updates

---
+++ precache-manifest.99f9500bd6dff68294624a2d1c5effed.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f93c9efba6dfc0f05c3c02938aeb3c",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ad92d4d8c52ff822647d",
+    "url": "/my-app/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "3df0c9137d16c56f668d",
+    "url": "/my-app/static/css/main.7e72930a.chunk.css"
+  },
+  {
+    "revision": "ad92d4d8c52ff822647d",
+    "url": "/my-app/static/js/2.b08a69fe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-app/static/js/2.b08a69fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3df0c9137d16c56f668d",
+    "url": "/my-app/static/js/main.939c81d2.chunk.js"
+  },
+  {
+    "revision": "5db4ee20b3bc870ebba1",
+    "url": "/my-app/static/js/runtime-main.3a315cac.js"
+  },
+  {
+    "revision": "dc51ffe02a1386801e69e89348c9b80e",
+    "url": "/my-app/static/media/pale.dc51ffe0.jpeg"
+  },
+  {
+    "revision": "85135756e622eb6e2213f1e977bb079b",
+    "url": "/my-app/static/media/photo1.85135756.jpg"
+  },
+  {
+    "revision": "dae51122692e2b96335cfd0fa13a8d2b",
+    "url": "/my-app/static/media/photo2.dae51122.jpg"
+  },
+  {
+    "revision": "c46d30839eae08e67ee6206486010d48",
+    "url": "/my-app/static/media/photo3.c46d3083.jpg"
+  },
+  {
+    "revision": "3ee3f7bca21c70441b8f09aef4723840",
+    "url": "/my-app/static/media/photo4.3ee3f7bc.jpg"
+  },
+  {
+    "revision": "6f9b35ba9b1108b3d7c88ebae1c5ff84",
+    "url": "/my-app/static/media/photo5.6f9b35ba.jpg"
+  },
+  {
+    "revision": "6aedeeb9f0303a68debaabd73af05add",
+    "url": "/my-app/static/media/photo6.6aedeeb9.jpg"
+  },
+  {
+    "revision": "7bc59b9c2f481ea3a4d16c3434dfeb08",
+    "url": "/my-app/static/media/portofolio.7bc59b9c.PNG"
+  }
+]);
